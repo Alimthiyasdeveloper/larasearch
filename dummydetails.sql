382d5badfd34f910bd549f524471de75a77978b0
Alimthiyasdeveloer <aimtiazsocial@gmail.com>
2020-08-21 17:55:21 +0530

    third commit

---
+++ dummydetails.sql
@@ -0,0 +1,272 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2020 at 01:46 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dummydetails`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dummydetails`
+--
+
+CREATE TABLE `dummydetails` (
+  `email` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dummydetails`
+--
+
+INSERT INTO `dummydetails` (`email`, `name`) VALUES
+('smartinez0@nydailynews.com', 'Sarah Martinez'),
+('bnelson1@independent.co.uk', 'Benjamin Nelson'),
+('kcruz2@multiply.com', 'Kelly Cruz'),
+('dmedina3@instagram.com', 'Deborah Medina'),
+('djackson4@youtu.be', 'Diana Jackson'),
+('nharrison5@yahoo.co.jp', 'Nicole Harrison'),
+('esmith6@phpbb.com', 'Evelyn Smith'),
+('pperkins7@ox.ac.uk', 'Paul Perkins'),
+('jkennedy8@t-online.de', 'Jennifer Kennedy'),
+('acarpenter9@chron.com', 'Ann Carpenter'),
+('lsimmonsa@yahoo.co.jp', 'Lillian Simmons'),
+('tlarsonob@booking.com', 'Terry Larson'),
+('rallenoc@lycos.com', 'Robin Allen'),
+('lramirezod@cbslocal.com', 'Lillian Ramirez'),
+('aholmesoe@dot.gov', 'Antonio Holmes'),
+('ebishopof@ehow.com', 'Earl Bishop'),
+('awilliamsog@wordpress.com', 'Antonio Williams'),
+('cwatsonoh@over-blog.com', 'Cheryl Watson'),
+('gdixonoi@globo.com', 'Gregory Dixon'),
+('pnicholsoj@columbia.edu', 'Phyllis Nichols'),
+('jrossok@hao123.com', 'Jeremy Ross'),
+('agarrettol@seesaa.net', 'Andrea Garrett'),
+('msmithom@foxnews.com', 'Margaret Smith'),
+('dleeon@jimdo.com', 'Diana Lee'),
+('ccollinsoo@icio.us', 'Christine Collins'),
+('rgrantop@comcast.net', 'Rachel Grant'),
+('rgrahamoq@aboutads.info', 'Richard Graham'),
+('jgilbertor@springer.com', 'Joan Gilbert'),
+('eandersonos@google.cn', 'Eugene Anderson'),
+('kfoxot@dion.ne.jp', 'Kathryn Fox'),
+('jjonesou@indiatimes.com', 'Jacqueline Jones'),
+('tpalmerov@answers.com', 'Teresa Palmer'),
+('fwillisow@google.com.br', 'Frank Willis'),
+('twrightox@dell.com', 'Thomas Wright'),
+('tfulleroy@devhub.com', 'Tina Fuller'),
+('egrayoz@paypal.com', 'Elizabeth Gray'),
+('sharperp0@csmonitor.com', 'Susan Harper'),
+('rgonzalesp1@multiply.com', 'Roy Gonzales'),
+('spricep2@behance.net', 'Stephen Price'),
+('jhansenp3@cafepress.com', 'Jack Hansen'),
+('apetersonp4@webs.com', 'Amanda Peterson'),
+('bnguyenp5@disqus.com', 'Billy Nguyen'),
+('sgibsonp6@goo.ne.jp', 'Sandra Gibson'),
+('spetersonp7@quantcast.com', 'Sean Peterson'),
+('alarsonp8@google.com.hk', 'Anthony Larson'),
+('jmurphyp9@jugem.jp', 'Jeffrey Murphy'),
+('arobertspa@istockphoto.com', 'Angela Roberts'),
+('prodriguezpb@aol.com', 'Philip Rodriguez'),
+('acoxpc@google.cn', 'Andrea Cox'),
+('agreenpd@cmu.edu', 'Alice Green'),
+('bfisherpe@pagesperso-orange.fr', 'Bonnie Fisher'),
+('jpricepf@abc.net.au', 'Joe Price'),
+('bwarrenpg@com.com', 'Brian Warren'),
+('sjonesph@bbc.co.uk', 'Susan Jones'),
+('erodriguezpi@cafepress.com', 'Earl Rodriguez'),
+('asanderspj@shareasale.com', 'Andrew Sanders'),
+('ptuckerpk@example.com', 'Patricia Tucker'),
+('afosterpl@yelp.com', 'Angela Foster'),
+('mlewispm@addtoany.com', 'Melissa Lewis'),
+('fkelleypn@reddit.com', 'Frank Kelley'),
+('pdunnpo@washington.edu', 'Phillip Dunn'),
+('lgrahampp@webs.com', 'Lillian Graham'),
+('klanepq@bravesites.com', 'Kathleen Lane'),
+('ahallpr@oakley.com', 'Andrew Hall'),
+('rhillps@thetimes.co.uk', 'Rebecca Hill'),
+('jarnoldpt@yale.edu', 'Jeffrey Arnold'),
+('pmoorepu@cloudflare.com', 'Patrick Moore'),
+('tvasquezpv@wiley.com', 'Timothy Vasquez'),
+('rkimpw@wikimedia.org', 'Rebecca Kim'),
+('rpowellpx@hatena.ne.jp', 'Randy Powell'),
+('wricepy@cbsnews.com', 'Wanda Rice'),
+('anelsonpz@taobao.com', 'Angela Nelson'),
+('dsmithq0@arstechnica.com', 'Dennis Smith'),
+('bweaverq1@sbwire.com', 'Bruce Weaver'),
+('rcunninghamq2@naver.com', 'Roger Cunningham'),
+('shuntq3@ftc.gov', 'Sean Hunt'),
+('mdanielsq4@woothemes.com', 'Maria Daniels'),
+('dsanchezq5@biglobe.ne.jp', 'David Sanchez'),
+('bmontgomeryq6@businesswire.com', 'Brandon Montgomery'),
+('cdunnq7@mail.ru', 'Catherine Dunn'),
+('kromeroq8@phoca.cz', 'Kathryn Romero'),
+('jdeanq9@booking.com', 'James Dean'),
+('bsandersqa@tumblr.com', 'Brandon Sanders'),
+('probertsonqb@cnet.com', 'Phyllis Robertson'),
+('jholmesqc@forbes.com', 'Joe Holmes'),
+('lmorganqd@utexas.edu', 'Lois Morgan'),
+('rdeanqe@globo.com', 'Ryan Dean'),
+('bfordqf@dmoz.org', 'Billy Ford'),
+('lwalkerqg@unicef.org', 'Linda Walker'),
+('mgonzalesqh@blogspot.com', 'Michelle Gonzales'),
+('dmartinezqi@sfgate.com', 'Debra Martinez'),
+('jwarrenqj@boston.com', 'Jason Warren'),
+('gmarshallqk@booking.com', 'Gary Marshall'),
+('clawrenceql@bloomberg.com', 'Cheryl Lawrence'),
+('sthomasqm@foxnews.com', 'Susan Thomas'),
+('srogersqn@vinaora.com', 'Steven Rogers'),
+('elawsonqo@wordpress.com', 'Elizabeth Lawson'),
+('jkennedyqp@reddit.com', 'Jeremy Kennedy'),
+('sgrantqq@ebay.com', 'Steven Grant'),
+('hmendozaqr@dedecms.com', 'Howard Mendoza'),
+('charveyqs@hc360.com', 'Carlos Harvey'),
+('rstanleyqt@cdc.gov', 'Ruth Stanley'),
+('bperryqu@ow.ly', 'Billy Perry'),
+('eryanqv@diigo.com', 'Ernest Ryan'),
+('acolemanqw@apache.org', 'Anne Coleman'),
+('hleeqx@wix.com', 'Howard Lee'),
+('blopezqy@comcast.net', 'Beverly Lopez'),
+('jkennedyqz@surveymonkey.com', 'Joyce Kennedy'),
+('dcookr0@altervista.org', 'Diana Cook'),
+('jjenkinsr1@wufoo.com', 'Joseph Jenkins'),
+('jgomezr2@addthis.com', 'Jimmy Gomez'),
+('fhowardr3@npr.org', 'Frances Howard'),
+('brichardsr4@who.int', 'Brenda Richards'),
+('thunterr5@cocolog-nifty.com', 'Thomas Hunter'),
+('rmoralesr6@blogger.com', 'Robert Morales'),
+('bharperr7@networksolutions.com', 'Bonnie Harper'),
+('ehernandezr8@twitter.com', 'Eugene Hernandez'),
+('lpiercer9@squarespace.com', 'Lori Pierce'),
+('pbishopra@163.com', 'Phillip Bishop'),
+('jbryantrb@over-blog.com', 'Judy Bryant'),
+('cdiazrc@diigo.com', 'Charles Diaz'),
+('cdavisrd@nationalgeographic.com', 'Cynthia Davis'),
+('jstanleyre@prlog.org', 'Jimmy Stanley'),
+('bporterrf@mlb.com', 'Bonnie Porter'),
+('dweaverrg@sitemeter.com', 'Doris Weaver'),
+('nandersonrh@answers.com', 'Nicole Anderson'),
+('ewatkinsri@ucoz.ru', 'Elizabeth Watkins'),
+('mdunnrj@amazon.de', 'Mark Dunn'),
+('mgonzalesrk@parallels.com', 'Matthew Gonzales'),
+('mhendersonrl@digg.com', 'Michelle Henderson'),
+('lpricerm@over-blog.com', 'Lisa Price'),
+('mwarrenrn@wordpress.com', 'Melissa Warren'),
+('slongro@flickr.com', 'Scott Long'),
+('rfernandezrp@narod.ru', 'Robert Fernandez'),
+('bweaverrq@cnn.com', 'Brenda Weaver'),
+('jgonzalesrr@icq.com', 'James Gonzales');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`migration`, `batch`) VALUES
+('2014_10_12_000000_create_users_table', 1),
+('2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'raj', 'raj@gmai.com', '12345678', NULL, NULL, NULL),
+(3, 'rajkum', 'raj@gmail.com', '12345678', NULL, NULL, NULL),
+(4, 'imty', 'imty@gmail.com', '2345678', NULL, NULL, NULL),
+(6, 'imty', 'imtyg@gmail.com', '2345678', NULL, NULL, NULL),
+(8, 'imtiaz', 'imtiaz@mail.com', '234566', NULL, NULL, NULL),
+(9, 'imtiaz', 'imtiazz@mail.com', '234566', NULL, NULL, NULL),
+(10, 'hais', 'aiis@@mail.com', '345345353535345', NULL, NULL, NULL),
+(12, 'hais', 'aidrs@@mail.com', '345345353535345', NULL, NULL, NULL),
+(13, 'vajid', 'vajid@mail.com', '45678988', NULL, NULL, NULL),
+(14, 'vajid', 'vajidyas@mail.com', '45678988', NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`),
+  ADD KEY `password_resets_token_index` (`token`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
